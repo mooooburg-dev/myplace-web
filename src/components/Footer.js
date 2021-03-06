831131cebfd8f002c500327b889bb29859161e7e
jeongmu.park <jeongmu.park@medit.com>
2021-03-06 18:51:48 +0900

    footer 컨트롤러 추가

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+	return (
+		<footer className="footer">
+			<div className="text-center">drawyourmind.com</div>
+		</footer>
+	)
+}
+
+export default Footer
