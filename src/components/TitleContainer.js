ce6f79f7779b882e0a1b8ddc50420a7d3a302069
jeongmu.park <jeongmu.park@medit.com>
2021-03-09 00:46:03 +0900

    title 문구 및 스타일 수장

--- src/components/TitleContainer.js
+++ src/components/TitleContainer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const TitleContainer = () => {
 	return (
 		<div className="title-container">
-			<h1 className="title-text text-center">PUNCHLINE</h1>
+			<div className="title-text text-center">Punch Line</div>
 		</div>
 	)
 }
