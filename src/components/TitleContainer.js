a7754ad4395d3bbe554c8b4e256d424a5ceaf462
jeongmu.park <jeongmu.park@medit.com>
2021-03-09 01:02:55 +0900

    로고, 타이틀 폰트 변경

--- src/components/TitleContainer.js
+++ src/components/TitleContainer.js
@@ -3,7 +3,11 @@ import React from 'react'
 const TitleContainer = () => {
 	return (
 		<div className="title-container">
-			<div className="title-text text-center">Punch Line</div>
+			{/* <div className="title-logo"></div> */}
+			<div className="title-text text-center">
+				<img src="/static/imgs/logo.svg" />
+				Punch Line
+			</div>
 		</div>
 	)
 }
