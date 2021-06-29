972a827359461328353185512b28a7651da65136
jeongmu.park <jeongmupark@gmail.com>
2021-06-29 14:30:53 +0900

    주석 제거

--- src/components/TitleContainer.js
+++ src/components/TitleContainer.js
@@ -3,7 +3,6 @@ import React from 'react'
 const TitleContainer = () => {
   return (
     <div className="title-container">
-      {/* <div className="title-logo"></div> */}
       <div className="title-text text-center">
         <img src="/static/imgs/logo.svg" />
         Punch Line
